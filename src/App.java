f58ba74234ff4157185c5718f6c0defc946cde43
Rudra-Sawant <rudra_sawant@persistent.co.in>
2019-08-22 12:19:23 +0530

    new changes

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java -- We are Awesome !!!!");
-        System.out.println("I am enjoying the training...");
+        System.out.println("I am not enjoying the training...");
     }
 }
